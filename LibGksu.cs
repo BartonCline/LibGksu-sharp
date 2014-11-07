d4a19d1a99084791727fb2c9fc02f17098172473
bcline3078 <barton@bcdesignswell.com>
2014-11-07 01:21:22 -0800

    Fix invocation documentation. Add GPL 2 header

--- LibGksu.cs
+++ LibGksu.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿//
+//  LibGksu.cs
+//
+//  Author:
+//       barton cline <myfullname at gmail.com>
+//
+//  Copyright (c) 2014 barton cline
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+
+using System;
 using System.Runtime.InteropServices;
 
 namespace LibGksu
@@ -352,7 +375,7 @@ namespace LibGksu
 			}
 
 			/// <summary>
-			/// Runs the sudo prompt after checking.. and saving.. .
+			/// Runs the sudo prompt; Does not check for a cached password with no option to cache it.
 			/// </summary>
 			/// <returns><c>true</c>, if fuller was run, <c>false</c> otherwise.</returns>
 			/// <param name="askPassFunc">Ask pass func.</param>
